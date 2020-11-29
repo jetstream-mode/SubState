acfc862d4dc86a1dcba8cbbf29cbd54ba082722f
JKnee1 <josh.kneedler@nike.com>
2020-11-28 20:37:30 -0800

    update

--- SubState/SubState/Views/SubState-Evaluator.swift
+++ SubState/SubState/Views/SubState-Evaluator.swift
@@ -25,11 +25,10 @@ extension SubState {
         //let users: [User]?
         
         // Elements
-        /*
         enum Element: EvaluatorElement {
-            case filterList
+            case trackList
         }
- */
+
         
         init() {
             //users = Bundle.main.decode([User].self, from: "users.json")
@@ -41,63 +40,25 @@ extension SubState {
 
 extension SubState.Evaluator {
     enum State: EvaluatorState {
-        case notStarted(NotStartedState)
-        case filtering(FilteringState)
+        case listTracks(ListTracksState)
+        case addLog(AddLogState)
+        case listLog(ListLogState)
     }
     
-    struct NotStartedState {
-        var buttonText: String
+    struct ListTracksState {
+        var pageTitle: String
+        //maybe
+        var tracks: [Tracks]
     }
-
-    struct FilteringState {
-        var backButtonText: String
-        var daisyButtonText: String
-        var resetButtonText: String
-        var userData: [User]
+    
+    struct AddLogState {
+        var pageTitle: String
+        //maybe
+        var tracks: [Tracks]
+    }
+    
+    struct ListLogState {
+        var pageTitle: String
     }
 }
 
-extension SubState.Evaluator: TextFieldEvaluating {
-    func viewDidAppear() {
-        if state == nil {
-            notStarted()
-        }
-    }
-    
-    func notStarted() {
-        state = .notStarted(
-            NotStartedState(
-                buttonText: "Filter Now"
-            )
-        )
-    }
-    
-    func start() {
-
-        guard let users = users else {
-            return
-        }
-
-        state = .filtering(
-            FilteringState(
-                backButtonText: "Back",
-                daisyButtonText: "Daisy",
-                resetButtonText: "Reset",
-                userData: users
-                )
-        )
-    }
-    
-    // pass through tests
-    func filterDaisy() {
-        translator.filterDaisy()
-    }
-    
-    func resetText() {
-        translator.resetText()
-    }
-    
-    func textFieldDidChange(text: String, elementName: EvaluatorElement) {
-        debugPrint("hey evaluator from text field ", text)
-    }
-}
--- SubState/SubState/Views/SubState-Translator.swift
+++ SubState/SubState/Views/SubState-Translator.swift
@@ -8,3 +8,56 @@
 import Combine
 import Foundation
 import SwiftUI
+
+extension SubState {
+    
+    class Translator {
+        
+        typealias Evaluator = SubState.Evaluator
+        typealias Element = Evaluator.Element
+        typealias State = Evaluator.State
+        
+        // view models
+        
+        // State Sink
+        var stateSink: AnyCancellable?
+        
+        init(_ state: Passable<State>) {
+            stateSink = state.subject.sink { [weak self] value in
+                if let value = value {
+                    self?.translate(state: value)
+                }
+            }
+        }
+        
+        deinit {
+            debugPrint("deinit filter translator")
+        }
+        
+    }
+}
+
+extension SubState.Translator {
+    func translate(state: State) {
+        switch state {
+        case .listTracks(let state):
+            translateListTracks(state)
+        case .addLog(let state):
+            translateAddLog(state)
+        case .listLog(let state):
+            translateListLog(state)
+        }
+    }
+    
+    func translateListTracks(_ state: Evaluator.ListTracksState) {
+        debugPrint("translate list tracks")
+    }
+    
+    func translateAddLog(_ state: Evaluator.AddLogState) {
+        debugPrint("translate add log")
+    }
+    
+    func translateListLog(_ state: Evaluator.ListLogState) {
+        debugPrint("list log state")
+    }
+}
